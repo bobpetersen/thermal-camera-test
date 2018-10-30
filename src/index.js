903d077abdc1d3ed436f109d987f2cc490c132c8
Bob Petersen <33petersen@gmail.com>
2018-10-30 13:28:02 -0500

    uilt redux file structures

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 import './index.css';
 import App from './components/App/App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -13,10 +12,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('react-root'),
 );
-
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
