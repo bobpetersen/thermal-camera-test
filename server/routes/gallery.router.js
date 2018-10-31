9778818f18f4fb2a633926d1e25d4d84f986b880
Bob Petersen <33petersen@gmail.com>
2018-10-31 09:18:00 -0500

    fixed typo

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // router.get('/', (req, res) => {
 //   console.log('Get gallery');
 //   // This is where we will make the postgres query for the gallery images
-//   // Below is my an example of my request for average levels from pon project
+//   // Below is my an example of my request for average levels from pond project
 //   const queryText = `SELECT CAST("tstz" AS "date") AS "dt", "avg"("level") AS "avg_level"
 //                      FROM "temp_level"
 //                      GROUP BY CAST("tstz" AS "date")
